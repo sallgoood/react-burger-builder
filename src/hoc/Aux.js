bd9f4f8d0c8a81776826c6ba681a188ad7557128
myoungso.kang <myoungso.kang@rakuten.com>
2019-07-15 17:06:09 +0900

    folder structure

--- src/hoc/Aux.js
+++
@@ -1,4 +0,0 @@
-const aux = (props) => props.children;
-
-//Higher order component approach
-export default aux;
---
+++ src/hoc/Aux/Aux.js
@@ -0,0 +1,4 @@
+const aux = (props) => props.children;
+
+//Higher order component approach which wraps other components
+export default aux;
