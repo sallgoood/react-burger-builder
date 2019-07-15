70ba658ac8c731b365d22a0ed78a388c0bcc40ec
myoungso.kang <myoungso.kang@rakuten.com>
2019-07-15 15:27:44 +0900

    creating responsive side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Aux from '../../hoc/Aux'
 import styles from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 //Higher order component approach
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <main className={styles.Content}>
             {props.children}
         </main>
