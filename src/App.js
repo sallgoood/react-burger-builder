53d99fc9479840fabab929aa853ecb2e7de2f909
myoungso.kang <myoungso.kang@rakuten.com>
2019-07-16 07:55:01 +0900

    Setting Up Routing & Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 
 function App() {
-  return (
-    <div>
-      <Layout>
-        <BurgerBuilder/>
-        <Checkout/>
-      </Layout>
-    </div>
-  );
+    return (
+        <div>
+            <Layout>
+                <Switch>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/" component={BurgerBuilder}/>
+                    <Checkout/>
+                </Switch>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
