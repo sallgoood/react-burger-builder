119cefb542ad912f5d7957c023d81801abe475ef
myoungso.kang <myoungso.kang@rakuten.com>
2019-07-16 12:43:00 +0900

    fix failed test case, Invariant failed: You should not use <NavLink> outside a <Router>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
@@ -7,16 +7,18 @@ import Orders from './containers/Orders/Orders'
 
 function App() {
     return (
-        <div>
-            <Layout>
-                <Switch>
-                    <Route path="/checkout" component={Checkout}/>
-                    <Route path="/orders" component={Orders}/>
-                    <Route path="/" component={BurgerBuilder}/>
-                    <Checkout/>
-                </Switch>
-            </Layout>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Layout>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/orders" component={Orders}/>
+                        <Route path="/" component={BurgerBuilder}/>
+                        <Checkout/>
+                    </Switch>
+                </Layout>
+            </div>
+        </BrowserRouter>
     );
 }
 
