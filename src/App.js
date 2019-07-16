968a7d96a0d84a116643b78702dd3cfc713b4827
myoungso.kang <myoungso.kang@rakuten.com>
2019-07-17 08:22:32 +0900

    Basic Redux Setup

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducer from './store/reducer'
+
+const store = createStore(reducer);
 
 function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout}/>
-                        <Route path="/orders" component={Orders}/>
-                        <Route path="/" component={BurgerBuilder}/>
-                        <Checkout/>
-                    </Switch>
-                </Layout>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <div>
+                    <Layout>
+                        <Switch>
+                            <Route path="/checkout" component={Checkout}/>
+                            <Route path="/orders" component={Orders}/>
+                            <Route path="/" component={BurgerBuilder}/>
+                            <Checkout/>
+                        </Switch>
+                    </Layout>
+                </div>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
