8150e7e3c1f681a04e59b774e5347a82d0e02db4
myoungso.kang <myoungso.kang@rakuten.com>
2019-07-17 09:28:32 +0900

    Preparing the Folder Structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import axios from '../../axios-orders'
 import Spinner from '../../components/UI/Spinner/Spinner'
 import withHttpErrorHandler from '../../hoc/withErrorHandler/withHttpErrorHandler'
 import {connect} from 'react-redux'
-import * as actionTypes from '../../store/actions'
+import * as actionTypes from '../../store/actions/actionTypes'
 
 class BurgerBuilder extends Component {
 
